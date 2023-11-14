4293ba5f3d1a52f265d0706baddc3c03a644b3da
Isaac Wilhite <isaacwilhite987@gmail.com>
2023-11-14 16:14:19 -0700

    solution

--- server/app.py
+++ server/app.py
@@ -6,3 +6,34 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:str>')
+def print_string(str):
+    print(str)
+    return str
+
+@app.route('/count/<int:int>')
+def count(int):
+    count = f''
+    for n in range(int):
+        count += f'{n}\n'
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        return 'Invalid operation. Please use +, -, *, div, or %'
